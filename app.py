9ff903bb4f06341aa88312b0ca211aaa383e5e8a
Hyeongjinkimm <126743666+Hyeongjinkimm@users.noreply.github.com>
2023-05-16 11:25:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ df = pd.DataFrame(
   columns=['x','y'])
 st.line_chart(df)
 
+
+
+import pandas as pd
+import numpy as np
+import streamlit as st
 df = pd.DataFrame(np.random.randn(500,2) / [50,50] + [37.76, -122.4],
 columns=['lat','lon']
 st.map(df)
