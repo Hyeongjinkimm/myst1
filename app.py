cea8fc0dc818b6790c6bd7a3e91bef2ae2b06ce8
Hyeongjinkimm <126743666+Hyeongjinkimm@users.noreply.github.com>
2023-05-16 11:21:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ st.write(x, 'squared is', x*x)
 st.sidebar.title("This is written inside the sidebar")
 st.sidebar.button("Click")
 st.sidebar.radio("Pick your gender",["Male","Female"])
+import pandas as pd
+import numpy as np
+df = pd.DataFrame(
+  np.random.randn(10,2),
+  columns=['x','y'])
+st.line_chart(df)
